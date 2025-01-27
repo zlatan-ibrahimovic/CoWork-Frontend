204b01ca0b4f03c64d8ff81fc452235031a9463b
souliyan <souliyan33@gmail.com>
2025-01-27 11:25:31 +0100

    US : 4 - Ajout Login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: '/signup', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulaire valide :', this.loginForm.value);
+    } else {
+      console.log('Formulaire invalide.');
+    }
+  }
+}
